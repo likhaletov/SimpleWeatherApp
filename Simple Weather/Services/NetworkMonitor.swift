76a426b729ab519b7bdb8fd67e0ca28e490237d5
Dmitry Likhaletov <likhaletov.d@gmail.com>
2020-08-07 08:24:57 +0300

    added network monitor

---
+++ Simple Weather/Services/NetworkMonitor.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkMonitor.swift
+//  Simple Weather
+//
+//  Created by Dmitry Likhaletov on 03.08.2020.
+//  Copyright © 2020 Dmitry Likhaletov. All rights reserved.
+//
+
+import Foundation
